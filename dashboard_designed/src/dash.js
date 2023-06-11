3e54445ed50b3266b5e13b1d19a784a1df836b63
Geetanjali <geetanjali.bgs@gmail.com>
2023-06-11 07:08:04 +0530

    dashboard integrated with backend

--- dashboard_designed/src/dash.js
+++ dashboard_designed/src/dash.js
@@ -1,7 +1,66 @@
-import React from "react";
+
+import React, { useEffect, useState } from 'react';
+
 import "bootstrap/dist/css/bootstrap.css";
 import "./dash.css";
 import styled from "styled-components";
+import dbData from "./db.json";
+console.log(dbData);
+var usersData = dbData["users"];
+// Function to calculate the medical needs count
+const calculateMedicalNeedsCount = () => {
+  let count = 0;
+  usersData.forEach((user) => {
+    count += user['medical history'];
+  });
+
+  return count;
+};
+
+// const data = [
+//   {
+//     "_id": {
+//       "$oid": "648496e3673f1994d8c2eea4"
+//     },
+//     "medical history": 20,
+//     "id": 0,
+//     "housing": 1,
+//     "sponsorship": 1
+//   },
+//  
+// ];
+
+const medicalNeedsCount = calculateMedicalNeedsCount(dbData);
+// console.log(medicalNeedsCount);
+
+// const medicalNeedsCount = dbData.reduce(
+//   (count, item) => (item['medical history'] ? count + 1 : count),0);
+// console.log(medicalNeedsCount);
+
+// const medicalHistoryCount = dbData.reduce((count, obj) => {
+//   if (obj.hasOwnProperty("medical history")) {
+//     return count + obj["medical history"];
+//   }
+//   return count;
+// }, 0);
+
+// console.log("Medical History Count:", medicalHistoryCount);
+
+
+
+// const [medicalHistoryCount, setMedicalHistoryCount] = useState(0);
+// useEffect(() => {
+//   fetch('http://localhost:8000/users')
+//     .then((response) => response.json())
+//     .then((data) => {
+//       // Calculate the count of medical history
+//       const count = data.reduce((total, item) => total + item['medical history'], 0);
+//       setMedicalHistoryCount(count);
+//     })
+//     .catch((error) => {
+//       console.error('Error fetching data:', error);
+//     })
+// }, []);
 const Container = styled.div`
   display: flex;
   align-items: center;
@@ -51,9 +110,18 @@ const Dash = () => {
         style={myclass}
         className=" field row g-4 py-5 row-cols-1 row-cols-lg-3 "
       >
+
+        {/* <div className="cont Box1">
+          <Feature title="Medical Needs" text="Count:  ${medicalNeedsCount}" data="Solved?" />
+        </div> */}
         <div className="cont Box1">
-          <Feature title="Medical Needs" text="Count: 1" data="Solved?" />
-        </div>
+        <Feature
+        title="Medical Needs"
+        text={`Count: ${medicalNeedsCount}`}
+        data="Solved?"
+        />
+</div>
+
 
         <div className="cont Box2">
           <Feature
